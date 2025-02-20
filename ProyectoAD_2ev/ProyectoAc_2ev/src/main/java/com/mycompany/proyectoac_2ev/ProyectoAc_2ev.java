89b038bca71e6013ddfa9b04f3d8f1fc578e912c
LAALAD <160270603+LAALAD@users.noreply.github.com>
2025-02-20 11:25:44 +0100

    ConexionORM

---
+++ ProyectoAD_2ev/ProyectoAc_2ev/src/main/java/com/mycompany/proyectoac_2ev/ProyectoAc_2ev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoac_2ev;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoAc_2ev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
