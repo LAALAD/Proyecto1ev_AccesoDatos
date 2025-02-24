67ca4fbc0097a64c4dcc0a316479c085f3f863c2
LAALAD <adrianaalonsoadrianzen@gmail.com>
2025-02-25 00:39:41 +0100

    leer?

--- ProyectoAD_2ev/ProyectoAc_2ev/src/main/java/IO/LeerXML.java
+++ ProyectoAD_2ev/ProyectoAc_2ev/src/main/java/IO/LeerXML.java
@@ -4,10 +4,70 @@
  */
 package IO;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import logicaNegocio.Jugador;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author DAM2_02
  */
 public class LeerXML {
-    
+
+    /**
+     * Método para cargar una lista de jugadores desde un fichero XML.
+     *
+     * @param ruta Ruta del fichero XML a cargar.
+     * @return Lista de objetos Jugador obtenidos del XML.
+     */
+    public static ArrayList<Jugador> leerJugadores() {
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        try {
+            // Cargar el fichero XML y parsearlo
+            //File file = new File(ruta);
+            File file = EscribirXML.seleccionarFichero();
+
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(file);
+            //doc.getDocumentElement().normalize(); //para que es esto??
+
+            // Se asume que el XML tiene un elemento raíz <jugadores> y varios elementos <jugador>
+            NodeList jugadoresList = doc.getElementsByTagName("jugador");
+            for (int i = 0; i < jugadoresList.getLength(); i++) {
+                Element jugadorElement = (Element) jugadoresList.item(i);
+             
+                // Leer los datos de cada jugador desde los nodos <id>, <nombre>, <partidasGanadas> y <partidasJugadas>
+                //String idStr = jugadorElement.getElementsByTagName("id").item(0).getTextContent();
+                String nombre = jugadorElement.getElementsByTagName("nombre").item(0).getTextContent();
+                String partidasGanadasStr = jugadorElement.getElementsByTagName("partidasGanadas").item(0).getTextContent();
+                String partidasJugadasStr = jugadorElement.getElementsByTagName("partidasJugadas").item(0).getTextContent();
+
+                // Crear el objeto Jugador (utilizando el constructor que recibe el nombre)
+                Jugador jugador = new Jugador(nombre);
+                // Asignar el id (opcional, en caso de que se necesite)
+                //jugador.setId_j(Integer.parseInt(idStr));
+                // Asignar estadísticas de partidas
+                jugador.setPartidasGanadas(Integer.parseInt(partidasGanadasStr));
+                jugador.setPartidasJugadas(Integer.parseInt(partidasJugadasStr));
+
+                // Agregar el jugador a la lista
+                jugadores.add(jugador);
+            }
+
+            System.out.println("XML de jugadores cargado desde: " + file.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jugadores;
+    }
 }
